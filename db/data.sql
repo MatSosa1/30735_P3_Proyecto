a18a7e65fe87ed3b1b67770143048b53e955afff
MatSosa1 <dmsosa3@espe.edu.ec>
2026-07-16 07:51:49 -0500

    Sample Data Fixed to ORM

--- db/data.sql
+++ db/data.sql
@@ -1,16 +1,16 @@
 -- Users
-INSERT INTO Users (name_user, surname_user, username_user, password_user) VALUES
+INSERT INTO "Users" (name_user, surname_user, username_user, password_user) VALUES
 ('Mateo', 'Sosa', 'msosa', '1234'),
 ('Admin', 'admin', 'admin', '1234');
 
 -- Roles
-INSERT INTO Roles (name_rol) VALUES
+INSERT INTO "Roles" (name_role) VALUES
 ('Vendedor'),
 ('Asistente'),
 ('Visitante'),
 ('Administrador');
 
-INSERT INTO Modules (id_module, name_module, url_module, parent_id_module) VALUES 
+INSERT INTO "Modules" (id_module, name_module, url_module, parent_id_module) VALUES 
 (1, 'Menu', NULL, NULL),
 (2, 'Submenu 1', NULL, 1),
 (3, 'Submenu 2', NULL, 1),
@@ -21,7 +21,7 @@ INSERT INTO Modules (id_module, name_module, url_module, parent_id_module) VALUE
 (8, 'Item Invisible', '/item_inv', 3);
 
 -- Users - Roles
-INSERT INTO Users_Roles VALUES
+INSERT INTO "Users_Roles" VALUES
 (1, 1),
 (1, 2),
 (2, 1),
@@ -30,7 +30,7 @@ INSERT INTO Users_Roles VALUES
 (2, 4);
 
 -- Roles - Modules
-INSERT INTO Roles_Modules VALUES
+INSERT INTO "Roles_Modules" VALUES
 -- Vendedor
 (1, 4),
 (1, 5),
