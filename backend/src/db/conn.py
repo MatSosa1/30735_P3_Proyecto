d1a9e6db89d8a40f25be7aa4a3464bdb3916fd2f
IMarcusDev <escobarmarcos511@gmail.com>
2026-07-20 22:35:02 -0500

    Remove dead commented-out code (SonarQube code smell)

--- backend/src/db/conn.py
+++ backend/src/db/conn.py
@@ -4,5 +4,4 @@ from src.config.env import DB_NAME, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_HOSTNA
 
 engine = create_engine(
   f'postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}',
-  # echo=True
 )
